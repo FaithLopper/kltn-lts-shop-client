2b1a4f5462ea247ad8f4d404a35724d26a4af167
Lô Minh Phát <lmpitmail0701@gmail.com>
2022-11-02 18:11:11 +0700

    fix env, add api

--- source/src/sagas/index.js
+++ source/src/sagas/index.js
@@ -1,8 +1,9 @@
 import { all } from 'redux-saga/effects';
-
+import categoryProduct from './categoryProduct'
 // import landing from "./landing"
 const sagas = [
     // ...landing,
+    ...categoryProduct
 ];
 
 function* rootSaga() {
